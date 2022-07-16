e2f45e73f537847874e6cdff451b180fb7385c7a
Keshav Shakti Singh <keshavshakti14@gmail.com>
2022-07-16 13:53:26 +0530

    done

--- diff.java
+++ diff.java
@@ -1,3 +1,9 @@
 public class diff {
-    
+    public static void main(String args[]){
+        int a = 25;
+        int b = 5;
+
+        int c = a-b;
+        System.out.println("difference is = "+c);
+    }
 }
